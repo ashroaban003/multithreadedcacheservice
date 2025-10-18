0907c03b4688743152ce08bb986b19f0dd109f82
ashroaban003 <ratnakumar48070@gmail.com>
2025-10-18 11:56:28 +0530

    initial commit

---
+++ src/main/java/com/example/multithreadedcacheservice/MultithreadedcacheserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreadedcacheservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedcacheserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadedcacheserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multithreadedcacheservice/MultithreadedcacheserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multithreadedcacheservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadedcacheserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
