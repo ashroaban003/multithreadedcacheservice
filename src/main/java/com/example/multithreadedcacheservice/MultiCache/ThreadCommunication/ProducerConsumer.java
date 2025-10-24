839520fc2318f340f3c5552b1f5bafd239e36491
ashroaban003 <ratnakumar48070@gmail.com>
2025-10-24 12:56:17 +0530

    final_producerconsumer

--- src/main/java/com/example/multithreadedcacheservice/MultiCache/ThreadCommunication/ProducerConsumer.java
+++ src/main/java/com/example/multithreadedcacheservice/MultiCache/ThreadCommunication/ProducerConsumer.java
@@ -4,31 +4,43 @@ class producer_consumer{
     private int data;
     Boolean isdata=false;
     public void consumer(){
-        synchronized(this){
-            while(!isdata){
-                wait();
+        for(int i=0;i<10;i++){
+            synchronized(this){  
+                try{
+                        while(!isdata){
+                            wait();
+                        }
+                        System.out.println("consumed value - " + data);
+                        isdata = false;
+                        notify();
+                }catch(Exception e){System.out.println("Error  while consuming");}
             }
-            System.out.println("current value - " + data);
-            isdata = false;
-            notify();
-        }
+       }
     }
     public void producer(){
         for(int i=0;i<10;i++){
             synchronized(this){
-                while(isdata){
-                    wait();
-                }
-                data = i;
-                isdata = true;
-                notify();
+                try{
+                    while(isdata){
+                        wait();
+                    }
+                    data = i;
+                    isdata = true;
+                    System.out.println("Sent " + i);
+                    notify();
+               }catch(Exception e){System.out.println("Error  while producing");}
             }
+            // System.out.println("Sent " + i);
         }
     }
 }
 public class ProducerConsumer {
 
     public static void main(String[] args) {
-        
+        producer_consumer pc = new producer_consumer();
+        Thread t1 = new Thread(()->pc.consumer());
+        Thread t2 = new Thread(()->pc.producer());
+        t1.start();
+        t2.start();
     }
 }
