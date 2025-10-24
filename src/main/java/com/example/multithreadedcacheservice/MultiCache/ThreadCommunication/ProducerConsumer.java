b2cc63197662bf4bcf7550b25d054a4eace76a70
ashroaban003 <ratnakumar48070@gmail.com>
2025-10-24 12:42:40 +0530

    classcreation

--- src/main/java/com/example/multithreadedcacheservice/MultiCache/ThreadCommunication/ProducerConsumer.java
+++ src/main/java/com/example/multithreadedcacheservice/MultiCache/ThreadCommunication/ProducerConsumer.java
@@ -1,5 +1,34 @@
 //package com.example.multithreadedcacheservice.MultiCache.ThreadCommunication;
 
+class producer_consumer{
+    private int data;
+    Boolean isdata=false;
+    public void consumer(){
+        synchronized(this){
+            while(!isdata){
+                wait();
+            }
+            System.out.println("current value - " + data);
+            isdata = false;
+            notify();
+        }
+    }
+    public void producer(){
+        for(int i=0;i<10;i++){
+            synchronized(this){
+                while(isdata){
+                    wait();
+                }
+                data = i;
+                isdata = true;
+                notify();
+            }
+        }
+    }
+}
 public class ProducerConsumer {
-    
+
+    public static void main(String[] args) {
+        
+    }
 }
