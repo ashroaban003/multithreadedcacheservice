6edda4593ab3783828d477fa3ffba11658fff8b6
ashroaban003 <ratnakumar48070@gmail.com>
2025-11-05 19:29:54 +0530

    constantThreadpool.

---
+++ src/main/java/com/example/multithreadedcacheservice/MultiCache/ThreadCommunication/ExecutorSercice01.java
@@ -0,0 +1,33 @@
+//package com.example.multithreadedcacheservice.MultiCache.ThreadCommunication;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorSercice01 {
+    public static void main(String[] args) {
+        ExecutorService ex = Executors.newFixedThreadPool(3);
+        long startTime = System.currentTimeMillis();
+        for(int i=0;i<10;i++){
+             int i0=i;
+            ex.submit(()->{
+              long x = factorial(i0);
+              System.out.println(x);
+            });
+        }
+        ex.shutdown();
+        try{
+            ex.awaitTermination(100, TimeUnit.SECONDS);
+        }catch(Exception e){
+            System.out.println("error");
+        }
+        System.out.println("Total Time - "+(System.currentTimeMillis()-startTime));
+    }
+    public static long factorial(int j){
+        long sum = 1;
+        for(int i=1;i<=j && i<=10; i++){
+            sum*=i;
+        }
+        return sum;
+    }
+}
